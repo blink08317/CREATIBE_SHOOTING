e284e8032af38c021a53a3476ad3c41dac2fd66d
柴崎貴正 <blink08317@yahoo.co.jp>
2013-07-17 18:10:29 +0900

    Revert "CREATIVE"
    This reverts commit 23d3b4e7a639781c15241a6ecdab4df85c417373.

--- Creative_Shooting_serial/define.h
+++
@@ -1,9 +0,0 @@
-#define PI 3.141592654f //円周率
-#define PBULLET_X 11	//
-#define PBULLET_Y 64	//Y軸方向への最大段数
-#define WNDW 800		//ウィンドウの幅
-#define WNDH 600		//ウィンドウの高さ
-#define STAGEW 800	//ステージの幅
-#define STAGEH 1066	//ステージの高さ
-#define StageTime1 60*1e3 //ステージ１の制限時間
-#define StageTime2 70*1e3
